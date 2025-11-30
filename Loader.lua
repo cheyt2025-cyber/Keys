d8a8340a7e12cb00d0c4daac689c5e431a66c457
cheyt2025-cyber <cheyt2025@gmail.com>
2025-12-01 02:01:51 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -20,10 +20,23 @@ end
 
 -- Your games
 local Games = {
-    [2788229376]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/main/Arsenal",     -- Arsenal
-    [12690025832] = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/main/BladeBall",   -- Blade Ball
-    [6403373529]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/main/SlapBattles", -- Slap Battles
-    -- Add more...
+    [99879949355467]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Army%20Factory",     -- Army Factory
+    [99421051519131] = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Color%20Game%20Inf",   -- Color Game
+    [129854327403392]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Brainrot%20morph%20or%20die", -- Brainrot morph or die
+    [79657240466394]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Container%20RNG",-- Container RNG.
+    [117579798602171]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Crush%20a%20Brainrots",-- Crush for Brainrot
+    [102867184397587]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Be%20a%20Hurricane",-- Be a hurricane.
+    [95885904866309]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Dig%20and%20hatch",-- Dig and hatch a brainrot.
+    [111972673427354]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Dont%20steal%20the%20Kpop",-- Don’t Steal the KPop Demon Hunters
+    [96716540422444]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Don%E2%80%99t%20Steal%20%20The%20Baddies",-- Don't Steal The Baddies
+    [109073199927285]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Escape%20The%20Tsunami",-- Escape the Tsunami
+    [136404558442020]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Kayak%20and%20surf",-- Kayak and surf ...
+    [116681772517483]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Mutate%20or%20Lose%20Brainrot",-- mutate or lose brainrot
+    [155615604]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Prison%20Life",-- Prison Life
+    [76137189788863]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Raft%20Tycoon",-- Raft Tycoon
+    [78949013360566]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Shoot%20a%20Brainrot%20New%20UPD",-- Shoot a Brainrot
+    [14911088043]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/The%20Ride%20Auto%20Farm%20Cash",-- The Ride
+    [168556275]  = "https://raw.githubusercontent.com/cheyt2025-cyber/Boss/refs/heads/main/Console",-- Console
 }
 
 ZaporiumKeySystem.new({
